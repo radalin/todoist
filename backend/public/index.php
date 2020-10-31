eac39910a1952d90a7743310b7d781138baf8ad1
roy simkes <roysimkes@gmail.com>
2020-10-31 13:23:31 +0300

    Added cors configuration

--- backend/public/index.php
+++ backend/public/index.php
@@ -60,4 +60,17 @@ $app->addRoutingMiddleware();
  */
 // $app->addErrorMiddleware(false, true, true);
 
+// Enable Cors
+$app->options('/{routes:.+}', function ($request, $response, $args) {
+    return $response;
+});
+
+$app->add(function ($request, $handler) {
+    $response = $handler->handle($request);
+    return $response
+        ->withHeader('Access-Control-Allow-Origin', '*')
+        ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+        ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+});
+
 $app->run();
